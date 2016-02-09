d700eb7e2391136a4f6fd7ee87a39c9ff4971362
bwilken <brad@ctopowered.com>
2016-02-09 09:58:37 -0600

    this is going to work

--- php/recipes/configure.rb
+++ php/recipes/configure.rb
@@ -21,4 +21,22 @@ node[:deploy].each do |application, deploy|
       File.exists?("#{deploy[:deploy_to]}/shared/config")
     end
   end
+
+  # write out opsworks.php
+  template "#{deploy[:deploy_to]}/current/application/config/database.php" do
+    cookbook 'php'
+    source 'opsworks.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :database => deploy[:database],
+      :memcached => deploy[:memcached],
+      :layers => node[:opsworks][:layers],
+      :stack_name => node[:opsworks][:stack][:name]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/current/application/config")
+    end
+  end
 end
