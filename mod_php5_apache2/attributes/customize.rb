1db068810b58e04b2e4e3a9b945e9f428fe14b1f
bwilken <brad@ctopowered.com>
2016-02-10 11:59:00 -0600

    chef config

--- mod_php5_apache2/attributes/customize.rb
+++ mod_php5_apache2/attributes/customize.rb
@@ -8,12 +8,21 @@
 
 # The following shows how to override the mod_php5_apache2 packages:
 #
-#normal[:mod_php5_apache2][:packages] = [ 'php-xml',
-#                                         'php-common',
-#                                         'php-xmlrpc',
-#                                         'php-devel',
-#                                         'php-gd',
-#                                         'php-cli',
-#                                         'php-pear-Auth-SASL',
-#                                         'php-mysql',
-#                                         'php-mcrypt' ]
+normal[:mod_php5_apache2][:packages] = [
+    "php-xml",
+    "php-common",
+    "php-xmlrpc",
+    "php-gd",
+    "php-cli",
+    "php-pear-Auth-SASL",
+    "php-mcrypt",
+    "php-pecl-memcache",
+    "php-pear",
+    "php-pear-XML-Parser",
+    "php-pear-DB",
+    "php-pear-HTML-Common",
+    "php",
+    "php-devel",
+    "php-pear-Mail",
+    "php-pear-Mail-Mime"
+  ]
